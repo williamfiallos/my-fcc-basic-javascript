cd661a2ca219fdf2645ed023f90e63e89d730b38
williamfiallos <wfiallos10@gmail.com>
2018-12-20 22:30:04 -0500

    starting freeCodeCamp practice exercises

---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log("testing...");
+// Basic JavaScript: Comment Your JavaScript Code
+/*Exercise:
+1. Create a // style comment that contains at least five letters.
+2. Create a '/*' '*'/ style comment that contains at least five letters. */
+
