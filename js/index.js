91412933a612dbacceaaefee7989f57a886d8946
williamfiallos <wfiallos10@gmail.com>
2018-12-24 00:44:41 -0500

    understanding uninitialized variables

--- js/index.js
+++ js/index.js
@@ -14,3 +14,14 @@ b = a;
 // Basic JavaScript: Initializing Variables with the Assignment Operator
 var a = 9;
 
+// Basic JavaScript: Understanding Uninitialized Variables
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
