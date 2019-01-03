1db9f8c6e8a0c2b7848a290d45399294b707bf60
williamfiallos <wfiallos10@gmail.com>
2019-01-03 15:00:09 -0500

    compound assignment with augmented subtraction

--- js/index.js
+++ js/index.js
@@ -90,3 +90,12 @@ a += 12;
 b += 9;
 c += 7;
 
+// Basic JavaScript: Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+// Only modify code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
