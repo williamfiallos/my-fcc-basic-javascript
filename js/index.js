9360b11a0d6950b1d38740a6856d3db6d17e7b2f
williamfiallos <wfiallos10@gmail.com>
2019-01-05 00:41:22 -0500

    compound assignment with augmented division

--- js/index.js
+++ js/index.js
@@ -108,3 +108,12 @@ a *= 5;
 b *= 3;
 c *= 10;
 
+// Basic JavaScript: Compound Assignment With Augmented Division
+var a = 48;
+var b = 108;
+var c = 33;
+// Only modify code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
