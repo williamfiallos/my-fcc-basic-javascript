016017b046fd41bc0e68555ae5b2b8016146d58b
williamfiallos <wfiallos10@gmail.com>
2019-01-13 18:56:42 -0500

    constructing strings with variables

--- js/index.js
+++ js/index.js
@@ -157,3 +157,11 @@ ourStr += "I come second.";
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
 
+// Basic JavaScript: Constructing Strings with Variables
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+// Only change code below this line
+var myName = "William Fiallos";
+var myStr = "My name is " + myName + " and I am well!";
+
