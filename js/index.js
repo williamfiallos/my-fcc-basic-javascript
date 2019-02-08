9056bcffc09bf154bbc3705c64bfce4c42252022
williamfiallos <wfiallos10@gmail.com>
2019-02-08 16:41:15 -0500

    manipulate arrays with pop()

--- js/index.js
+++ js/index.js
@@ -288,3 +288,15 @@ ourArray.push(["happy", "joy"]);
 var myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line.
 myArray.push(["dog", 3]);
+
+// Basic JavaScript: Manipulate Arrays With pop()
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); 
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
+removedFromMyArray;
+
