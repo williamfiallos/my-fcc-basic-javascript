e74ccd578df3b7068b014697d911f48e8a7ec822
williamfiallos <wfiallos10@gmail.com>
2019-02-26 22:44:21 -0500

    global scope and functions

--- js/index.js
+++ js/index.js
@@ -350,3 +350,22 @@ function functionWithArgs(a, b){
 }
 functionWithArgs(5, 5);
 
+// Basic JavaScript: Global Scope and Functions
+// Declare your variable here
+var myGlobal = 10;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
