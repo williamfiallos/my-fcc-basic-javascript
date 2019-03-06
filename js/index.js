5aa6e1811da400491e12be91cb57aabc6f35ab11
williamfiallos <wfiallos10@gmail.com>
2019-03-05 20:49:48 -0500

    global vs local scope in functions

--- js/index.js
+++ js/index.js
@@ -381,3 +381,14 @@ myLocalScope();
 console.log(myVar);
 // Now remove the console log line to pass the test
 
+// Basic JavaScript: Global vs. Local Scope in Functions
+// Setup
+var outerWear = "T-Shirt";
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater"; 
+  // Only change code above this line
+  return outerWear;
+}
+myOutfit();
+
