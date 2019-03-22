7576cc0d5dc60c8dbb4935fb0347cd73787d734e
williamfiallos <wfiallos10@gmail.com>
2019-03-21 22:30:32 -0400

    return a value from a function with return

--- js/index.js
+++ js/index.js
@@ -392,3 +392,13 @@ function myOutfit() {
 }
 myOutfit();
 
+// Basic JavaScript: Return a Value from a Function with Return
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+console.log(timesFive(5));
