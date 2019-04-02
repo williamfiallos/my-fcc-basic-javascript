db43bb26d78119081cb971f31753978fce863ac0
williamfiallos <wfiallos10@gmail.com>
2019-04-01 20:40:45 -0400

    assignment with a returned value

--- js/index.js
+++ js/index.js
@@ -416,3 +416,19 @@ function addFive() {
 }
 // Only change code above this line
 var returnedValue = addFive();
+
+// Basic JavaScript: Assignment with a Returned Value
+// Example
+var changed = 0;
+function change(num) {
+  return (num + 5) / 3;
+}
+changed = change(10);
+// Setup
+var processed = 0;
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7);
+
