90abcc3c7383107af6dd3a6e3f5bba0dc132efed
williamfiallos <wfiallos10@gmail.com>
2019-04-16 21:53:05 -0400

    comparison with the inequality operator

--- js/index.js
+++ js/index.js
@@ -505,3 +505,14 @@ function compareEquality(a, b) {
 }
 // Change this value to test
 compareEquality(10, "10");
+
+// Basic JavaScript: Comparison with the Inequality Operator
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+// Change this value to test
+testNotEqual(10);
