92687f46283b24767036e9ae655299bfe546b750
williamfiallos <wfiallos10@gmail.com>
2019-04-18 21:33:28 -0400

    comparison with the strict inequality operator

--- js/index.js
+++ js/index.js
@@ -516,3 +516,16 @@ function testNotEqual(val) {
 }
 // Change this value to test
 testNotEqual(10);
+
+// Basic JavaScript: Comparison with the Strict Inequality Operator
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+  if (val !== 17) {
+  // Only Change Code Above this Line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+// Change this value to test
+testStrictNotEqual(10);
