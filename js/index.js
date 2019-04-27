adfc1bbef39399fec506b2908cc1e63df4734fb5
williamfiallos <wfiallos10@gmail.com>
2019-04-26 21:33:10 -0400

    comparison with the less than or equal to operator

--- js/index.js
+++ js/index.js
@@ -568,3 +568,17 @@ function testLessThan(val) {
 }
 // Change this value to test
 testLessThan(10);
+
+// Basic JavaScript: Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+// Change this value to test
+testLessOrEqual(10);
+
