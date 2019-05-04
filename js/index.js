2209c352b11ab088c7e6872f68778368944512ba
williamfiallos <wfiallos10@gmail.com>
2019-05-03 23:54:05 -0400

    introducing else if statements

--- js/index.js
+++ js/index.js
@@ -620,3 +620,16 @@ function testElse(val) {
 }
 // Change this value to test
 testElse(4);
+
+// Basic JavaScript: Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }
+}
+// Change this value to test
+testElseIf(7);
