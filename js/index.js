b9a5eb63a4f5ddbcce887160d54717c89e04b767
williamfiallos <wfiallos10@gmail.com>
2019-05-22 21:02:38 -0400

    selecting from many options with switch statements

--- js/index.js
+++ js/index.js
@@ -694,3 +694,27 @@ function golfScore(par, strokes) {
 // Change these values to test
 golfScore(5, 4);
 
+// Basic JavaScript: Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+      return "alpha";
+    break;
+    case 2:
+      return "beta";
+    break;
+    case 3:
+      return "gamma";
+    break;
+    case 4:
+      return "delta";
+    break;
+  }
+  // Only change code above this line  
+  return answer;  
+}
+// Change this value to test
+caseInSwitch(1);
+
