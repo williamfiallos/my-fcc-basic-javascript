33cfcfaa98c68a3050c5bbdfbd5d3cffbadc0902
williamfiallos <wfiallos10@gmail.com>
2019-05-23 20:49:27 -0400

    adding default option in switch statements

--- js/index.js
+++ js/index.js
@@ -718,3 +718,27 @@ function caseInSwitch(val) {
 // Change this value to test
 caseInSwitch(1);
 
+// Basic JavaScript: Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+    return "apple";
+    break;
+    case "b":
+    return "bird";
+    break;
+    case "c":
+    return "cat";
+    break;
+    default:
+    return "stuff";
+    break;
+  }
+  // Only change code above this line  
+  return answer;  
+}
+// Change this value to test
+switchOfStuff(1);
+
