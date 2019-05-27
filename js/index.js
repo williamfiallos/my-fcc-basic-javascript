203bde83b7b55332594b9b1645eda2402a97e22e
williamfiallos <wfiallos10@gmail.com>
2019-05-27 19:32:57 -0400

    replacing if else chains with switch - solved

--- js/index.js
+++ js/index.js
@@ -771,3 +771,31 @@ function sequentialSizes(val) {
 sequentialSizes(1);
 
 // Basic JavaScript: Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){ 
+    case "bob": 
+      return "Marley";
+      break;
+    case 42:
+      return "The Answer";
+      break;
+    case 1:
+      return "There is no #1";
+      break;
+    case 99: 
+      return "Missed me by this much!";
+      break;
+    case 7:
+      return "Ate Nine";
+      break;
+    default:
+      return "";
+      break;
+  }
+  // Only change code above this line  
+  return answer;  
+}
+// Change this value to test
+chainToSwitch(7);
