caaf0d38f393527fd12e274e423f88445d3bff12
williamfiallos <wfiallos10@gmail.com>
2019-06-04 22:04:06 -0400

    accessing object properties with dot notation

--- js/index.js
+++ js/index.js
@@ -870,3 +870,15 @@ var myDog = {
   "tails": 1,
   "friends": ["Rambo", "Muneca"]
 };
+
+// Basic JavaScript: Accessing Object Properties with Dot Notation
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+// Only change code below this line
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
