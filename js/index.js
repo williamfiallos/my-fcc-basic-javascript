d9894a441e49af32fd8d2d642304fdfa540e93fe
williamfiallos <wfiallos10@gmail.com>
2019-06-08 18:45:27 -0400

    add new properties to a javascript object

--- js/index.js
+++ js/index.js
@@ -925,3 +925,24 @@ myDog.name = "Happy Coder";
 // or
 myDog["name"] = "Happy Coder";
 
+// Basic JavaScript: Add New Properties to a JavaScript Object
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+ourDog.bark = "bow-wow";
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line.
+myDog.bark = "woof-woof";
+// or
+myDog["bark"] = "woof-woof";
+
