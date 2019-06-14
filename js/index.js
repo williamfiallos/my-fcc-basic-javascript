e0aade47b54facb64c8cdbdaa8e0bf58e90d2f22
williamfiallos <wfiallos10@gmail.com>
2019-06-13 21:48:19 -0400

    manipulating complex objects

--- js/index.js
+++ js/index.js
@@ -1011,3 +1011,29 @@ function checkObj(checkProp) {
 // Test your code by modifying these values
 checkObj("gift");
 
+// Basic JavaScript: Manipulating Complex Objects
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Blink 182",
+    "title": "Enema of the State",
+    "release_year": 1999,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ]
+  }
+  // Add record here
+];
+
