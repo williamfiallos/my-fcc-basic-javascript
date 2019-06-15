0dd9379d70907dc3d058274038dda447f80ede56
williamfiallos <wfiallos10@gmail.com>
2019-06-15 12:27:25 -0400

    accessing nested arrays

--- js/index.js
+++ js/index.js
@@ -1052,3 +1052,26 @@ var myStorage = {
 };
 var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
 
+// Basic JavaScript: Accessing Nested Arrays
+// Setup
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+// Only change code below this line
+var secondTree = myPlants[1].list[1]; // Change this line
+
