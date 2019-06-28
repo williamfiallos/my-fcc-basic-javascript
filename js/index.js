d86e7d166ed6ba33541fd2fb53559defc0211c3a
williamfiallos <wfiallos10@gmail.com>
2019-06-27 23:42:56 -0400

    nesting for loops -unresolved

--- js/index.js
+++ js/index.js
@@ -1265,3 +1265,17 @@ for (var i = 0; i < myArr.length; i++) {
 }
 
 // Basic JavaScript: Nesting For Loops
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+  // Only change code above this line
+  return product;
+}
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
