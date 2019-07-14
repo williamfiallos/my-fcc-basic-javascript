dad80efb2b4308b5e0026007679e4e32a9ae0b14
williamfiallos <wfiallos10@gmail.com>
2019-07-14 15:11:13 -0400

    compare scopes of the var and let keywords -solved

--- js/es6.js
+++ js/es6.js
@@ -98,3 +98,15 @@ catTalk();
 // Be certain not to use the var keyword anywhere in your code.
 // This exercise is designed to illustrate the difference between how var and let keywords assign scope to the declared variable. 
 // When programming a function similar to the one used in this exercise, it is often better to use different variable names to avoid confusion.
+function checkScope() {
+  "use strict";
+    let i = "function scope";
+    if (true) {
+      let i = "block scope";
+      console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+  } // checkScope() // returns "function scope"
+
+  
